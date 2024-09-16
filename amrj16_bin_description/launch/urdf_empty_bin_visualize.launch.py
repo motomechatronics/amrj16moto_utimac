885553cb287d2a3a6e4354e4c3b95af750db46e4
motomechatronics <salvatore.volpe@mtmt.it>
2024-09-16 20:32:12 +0000

    Add empty bin description and Gazebo display

---
+++ amrj16_bin_description/launch/urdf_empty_bin_visualize.launch.py
@@ -0,0 +1,70 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+import xacro
+
+# this is the function launch  system will look for
+def generate_launch_description():
+
+    ####### DATA INPUT ##########
+    #urdf_file = 'amrj16.urdf'
+    #xacro_file = "amrj16_main.xacro"
+    xacro_file = "amrj16_empty_bin.xacro"
+    package_description = "amrj16_bin_description"    
+    # Boolean flag to enable or disable joint_state_publisher_gui
+    use_joint_state_publisher_gui = False  # Set this to False if you don't want to launch it
+
+    ####### DATA INPUT END ##########
+    print("Fetching URDF ==>")    
+    #robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
+    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", xacro_file)
+
+    
+    rsp = Node(   
+        namespace="empty_bin",    
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher', 
+        emulate_tty=True,       
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
+        output="screen"
+        
+    )
+
+    nodes_to_launch = [rsp]
+
+       
+    # Robot State Publisher
+    # Joint State Publisher_gui
+
+    if use_joint_state_publisher_gui:
+        joint_state_publisher_gui_node = Node(
+            namespace="empty_bin", 
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',            
+            name='joint_state_publisher_node',
+            emulate_tty=True,
+        output="screen"
+        )
+        nodes_to_launch.append(joint_state_publisher_gui_node)
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'amrj16_rviz2_config.rviz')
+
+
+    rviz2_node = Node(  
+                     
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',            
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir]
+    )
+
+    nodes_to_launch.append(rviz2_node)
+
+    # create and return launch description object
+    return LaunchDescription(nodes_to_launch)
---
+++ amrj16_gazebo/launch/spawn_empty_bin_description.launch.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import random
+
+from launch_ros.actions import Node
+from launch import LaunchDescription
+
+
+# this is the function launch  system will look for
+
+
+def generate_launch_description():
+
+
+    # Position and orientation
+    # [X, Y, Z]
+    position = [0, 0, 6]
+    # [Roll, Pitch, Yaw]
+    orientation = [0.0, 0.0, 0]
+    # Base Name or robot
+    robot_base_name = "empty_bin"
+
+
+    entity_name = robot_base_name
+
+    # Spawn ROBOT Set Gazebo
+    spawn_empty_bin = Node(       
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='spawn_entity',
+        output='screen',
+        arguments=['-entity',
+                   entity_name,
+                   '-x', str(position[0]), '-y', str(position[1]), '-z', str(position[2]),
+                   '-R', str(orientation[0]), '-P', str(orientation[1]), '-Y', str(orientation[2]),
+                   '-topic', 'empty_bin/robot_description'
+                   ]
+    )
+
+    
+
+    # create and return launch description object
+    return LaunchDescription(
+        [
+            spawn_empty_bin,
+        ]
+    )
